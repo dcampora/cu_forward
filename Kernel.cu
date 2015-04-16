cbf43d274072c9a43740b78d096c63fbddd66d29
Daniel Campora <danielcampora@gmail.com>
2015-04-16 14:49:22 +0900

    Trying things.

--- Kernel.cu
+++ Kernel.cu
@@ -97,7 +97,7 @@ __device__ float fitHitToTrack(const float tx, const float ty, const Hit& h0, co
 __global__ void searchByTriplet(Track* const dev_tracks, const char* const dev_input,
   int* const dev_tracks_to_follow,
   bool* const dev_hit_used, int* const dev_atomicsStorage, Track* const dev_tracklets,
-  int* const dev_weak_tracks, int* const dev_event_offsets, int* const dev_hit_offsets, float* const dev_best_fits
+  int* const dev_weak_tracks, int* const dev_event_offsets, int* const dev_hit_offsets, float* const dev_best_fits,
   int* const dev_misc) {
   
   /* Data initialization */
