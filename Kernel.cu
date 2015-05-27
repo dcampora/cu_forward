291812e4dba9a8b4499764bc6c2bb9bfac344581
dcampora <dcampora@lab14.lbdaq.cern.ch>
2015-05-27 14:18:35 +0200

    Made hit_h2_candidates dependent on h1 instead of h0, made it faster.

--- Kernel.cu
+++ Kernel.cu
@@ -56,9 +56,15 @@ __device__ void fillCandidates(int* const hit_candidates,
 
   const int blockDim_product = blockDim.x * blockDim.y;
   int first_sensor = number_of_sensors - 1;
-  while (first_sensor >= 4) {
+  while (first_sensor >= 2) {
     const int second_sensor = first_sensor - 2;
-    const int third_sensor = first_sensor - 4;
+
+    const bool process_h1_candidates = first_sensor >= 4;
+    const bool process_h2_candidates = first_sensor <= number_of_sensors - 3;
+
+    // Sensor dependent calculations
+    const int z_s0 = process_h2_candidates ? sensor_Zs[first_sensor + 2] : 0;
+    const int z_s2 = process_h2_candidates ? sensor_Zs[second_sensor] : 0;
 
     // Iterate in all hits in z0
     for (int i=0; i<((int) ceilf( ((float) sensor_hitNums[first_sensor]) / blockDim_product)); ++i) {
@@ -66,92 +72,88 @@ __device__ void fillCandidates(int* const hit_candidates,
       bool inside_bounds = h0_element < sensor_hitNums[first_sensor];
 
       if (inside_bounds) {
-        bool first_h1_found = false;
-        bool last_h1_found = false;
+        bool first_h1_found = false, last_h1_found = false;
+        bool first_h2_found = false, last_h2_found = false;
         const int h0_index = sensor_hitStarts[first_sensor] + h0_element;
         int h1_index;
         const Hit h0 {hit_Xs[h0_index], 0.f, hit_Zs[h0_index]};
-        Hit h1_first, h1;
-        
-        // Iterate in all hits in z1
-        for (int h1_element=0; h1_element<sensor_hitNums[second_sensor]; ++h1_element) {
-          inside_bounds = h1_element < sensor_hitNums[second_sensor];
+        const int hitstarts_s2 = sensor_hitStarts[second_sensor];
+        const int hitnums_s2 = sensor_hitNums[second_sensor];
 
-          if (inside_bounds) {
-            h1_index = sensor_hitStarts[second_sensor] + h1_element;
-            h1.x = hit_Xs[h1_index];
-            h1.z = hit_Zs[h1_index];
-
-            // Check if h0 and h1 are compatible
-            const float h_dist = fabs(h1.z - h0.z);
-            const float dxmax = PARAM_MAXXSLOPE * h_dist;
-            const bool tol_condition = fabs(h1.x - h0.x) < dxmax;
-            
-            // Find the first one
-            if (!first_h1_found && tol_condition) {
-              hit_candidates[2 * h0_index] = h1_index;
-              first_h1_found = true;
-              h1_first = h1;
-            }
-            // The last one, only if the first one has already been found
-            else if (first_h1_found && !tol_condition) {
-              // hit_candidates[2 * h0_index + 1] = h1_index;
-              last_h1_found = true;
-              break;
-            }
-          }
-        }
-
-        // Note: If first is not found, then both should be -1
-        // and there wouldn't be any iteration
-        if (first_h1_found) {
-          hit_candidates[2 * h0_index + 1] = h1_index + (last_h1_found ? 0 : 1);
-
-          // We also fill in the h2 candidates here
-          // Note: h1 contains the last h1 found at this moment
-          bool first_h2_found = false;
-          bool last_h2_found = false;
-          int h2_index;
-
-          // Calculate the range for the third sensor, using third sensor's generic Z
-          const int z_sensor = sensor_Zs[third_sensor];
-          float z2_tz = (((float) z_sensor) - h0.z) / (h1_first.z - h0.z);
-          float x = h0.x + (h1_first.x - h0.x) * z2_tz;
-          const float xmin_h2 = x - PARAM_TOLERANCE;
-
-          z2_tz = (((float) z_sensor) - h0.z) / (h1.z - h0.z);
-          x = h0.x + (h1.x - h0.x) * z2_tz;
-          const float xmax_h2 = x + PARAM_TOLERANCE;
+        float xmin_h2, xmax_h2;
+        if (process_h2_candidates) {
+          // Note: Here, we take h0 as if it were h1, the rest
+          // of the notation is fine.
           
-          // Iterate in all hits in z2
-          for (int h2_element=0; h2_element<sensor_hitNums[third_sensor]; ++h2_element) {
-            inside_bounds = h2_element < sensor_hitNums[third_sensor];
+          // Min and max possible x0s
+          const float h_dist = fabs(h0.z - z_s0);
+          const float dxmax = PARAM_MAXXSLOPE * h_dist;
+          const float x0_min = h0.x - dxmax;
+          const float x0_max = h0.x + dxmax;
+
+          // Min and max possible h1s for that h0
+          float z2_tz = (((float) z_s2 - z_s0)) / (h0.z - z_s0);
+          float x = x0_max + (h0.x - x0_max) * z2_tz;
+          xmin_h2 = x - PARAM_TOLERANCE;
+
+          x = x0_min + (h0.x - x0_min) * z2_tz;
+          xmax_h2 = x + PARAM_TOLERANCE;
+        }
+        
+        if (first_sensor >= 4) {
+          // Iterate in all hits in z1
+          for (int h1_element=0; h1_element<hitnums_s2; ++h1_element) {
+            inside_bounds = h1_element < hitnums_s2;
+
             if (inside_bounds) {
+              h1_index = hitstarts_s2 + h1_element;
+              const Hit h1 {hit_Xs[h1_index], 0.f, hit_Zs[h1_index]};
 
-              h2_index = sensor_hitStarts[third_sensor] + h2_element;
-              const Hit h2 {hit_Xs[h2_index], 0.f, hit_Zs[h2_index]};
-
-              if (!first_h2_found) {
-                if (h2.x > xmin_h2) {
-                  hit_h2_candidates[2 * h0_index] = h2_index;
-                  first_h2_found = true;
+              if (process_h1_candidates && !last_h1_found) {
+                // Check if h0 and h1 are compatible
+                const float h_dist = fabs(h1.z - h0.z);
+                const float dxmax = PARAM_MAXXSLOPE * h_dist;
+                const bool tol_condition = fabs(h1.x - h0.x) < dxmax;
+                
+                // Find the first one
+                if (!first_h1_found && tol_condition) {
+                  hit_candidates[2 * h0_index] = h1_index;
+                  first_h1_found = true;
+                }
+                // The last one, only if the first one has already been found
+                else if (first_h1_found && !tol_condition) {
+                  hit_candidates[2 * h0_index + 1] = h1_index;
+                  last_h1_found = true;
                 }
               }
 
-              else {
-                if (h2.x > xmax_h2) {
+              if (process_h2_candidates && !last_h2_found) {
+                if (!first_h2_found && h1.x > xmin_h2) {
+                  hit_h2_candidates[2 * h0_index] = h1_index;
+                  first_h2_found = true;
+                }
+                else if (first_h2_found && h1.x > xmax_h2) {
+                  hit_h2_candidates[2 * h0_index + 1] = h1_index;
                   last_h2_found = true;
                 }
+              }
+
+              if ((!process_h1_candidates || last_h1_found) &&
+                  (!process_h2_candidates || last_h2_found)) {
+                break;
               }
             }
           }
 
           // Note: If first is not found, then both should be -1
           // and there wouldn't be any iteration
-          if (first_h2_found) {
-            hit_h2_candidates[2 * h0_index + 1] = h2_index + (last_h2_found ? 0 : 1);
+          if (process_h1_candidates && first_h1_found && !last_h1_found) {
+            hit_candidates[2 * h0_index + 1] = hitstarts_s2 + hitnums_s2;
           }
 
+          if (process_h2_candidates && first_h2_found && !last_h2_found) {
+            hit_h2_candidates[2 * h0_index + 1] = hitstarts_s2 + hitnums_s2;
+          }
         }
       }
     }
@@ -497,12 +499,6 @@ __global__ void searchByTriplet(Track* const dev_tracks, const char* const dev_i
           const int last_h1 = hit_candidates[2 * h0_index + 1];
           num_h1_to_process = last_h1 - first_h1;
           atomicMax(max_numhits_to_process, num_h1_to_process);
-
-          first_h2 = hit_h2_candidates[2 * h0_index];
-          last_h2 = hit_h2_candidates[2 * h0_index + 1];
-          // In case there be no h2 to process,
-          // we can preemptively prevent further processing
-          inside_bounds &= first_h2 != -1;
         }
 
         __syncthreads();
@@ -522,6 +518,12 @@ __global__ void searchByTriplet(Track* const dev_tracks, const char* const dev_i
               h1.y = hit_Ys[h1_index];
               h1.z = hit_Zs[h1_index];
             }
+
+            first_h2 = hit_h2_candidates[2 * h1_index];
+            last_h2 = hit_h2_candidates[2 * h1_index + 1];
+            // In case there be no h2 to process,
+            // we can preemptively prevent further processing
+            inside_bounds &= first_h2 != -1;
           }
 
           // Iterate in the third list of hits
