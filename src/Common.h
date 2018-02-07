f34017ba4efb9ed7155bc6a0b72edc24b5c5060f
Daniel Hugo Campora Perez <dcampora@ibmpower-1.dyndns.cern.ch>
2018-02-07 11:51:24 +0100

    Removed check in data deserialization.

--- src/Common.h
+++ src/Common.h
@@ -27,8 +27,7 @@ struct EventInfo {
   EventInfo() = default;
   EventInfo(const std::vector<uint8_t>& event) {
     uint8_t* input = (uint8_t*) event.data();
-    uint8_t* end = input + event.size();
-
+    
     numberOfSensors  = *((int32_t*)input); input += sizeof(int32_t);
     numberOfHits     = *((int32_t*)input); input += sizeof(int32_t);
     sensor_Zs        = (int32_t*)input; input += sizeof(int32_t) * numberOfSensors;
@@ -38,10 +37,5 @@ struct EventInfo {
     hit_Xs           = (float*)  input; input += sizeof(float)   * numberOfHits;
     hit_Ys           = (float*)  input; input += sizeof(float)   * numberOfHits;
     hit_Zs           = (float*)  input; input += sizeof(float)   * numberOfHits;
-
-    if (input != end) {
-      std::cerr << "Failed to deserialize events" << std::endl;
-      exit(-1);
-    }
   }
 };
